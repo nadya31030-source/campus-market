7e7d3fd0a2fe7765ddcefe89f201eeb175a970bc
nadya31030-source <nadya31030@gmail.com>
2025-12-07 19:15:09 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -904,7 +904,8 @@ def create_app(test_config=None):
 
     return app
 
+app = create_app()
+
 # ---------- Run ----------
 if __name__ == '__main__':
-    app = create_app()
     app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000)), debug=True)
